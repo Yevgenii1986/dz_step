c81523327a86a8e2f758b555e6d1e8fa31989a20
Yevgenii1986 <wwwprodakshen@gmail.com>
2023-12-06 10:44:42 +0100

    функция вывода среднего числа

--- dz_def.py
+++ dz_def.py
@@ -5,3 +5,14 @@ def calculate_sum(number_one, number_two):
 
 result = calculate_sum(3, 4)
 print(result)
+
+my_list = [1, 5, 6, 55, 8, 3]
+
+
+def calculate_average(nam):
+    res = sum(nam) / len(nam)
+    return res
+
+
+result = calculate_average(my_list)
+print(result)
